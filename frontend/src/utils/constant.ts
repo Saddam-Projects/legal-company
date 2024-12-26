13401f1f5217939128a70f10942a05ad9a424fcd
saddam-satria <ardhisaddam@gmail.com>
2024-12-26 16:57:31 +0700

    fix: dockerfile

--- frontend/src/utils/constant.ts
+++ frontend/src/utils/constant.ts
@@ -1,6 +1,6 @@
 export const THEME_KEY = btoa('theme');
 export const TITLE = 'PT. Arunika Indo Miratama';
-export const BASE_API_URL = (process.env.BASE_API_URL as string) || 'http://127.0.0.1:8000';
+export const BASE_API_URL = (process.env.NEXT_PUBLIC_BASE_API_URL as string) || 'http://127.0.0.1:8080';
 export const DESCRIPTION = 'Legal Expertise Work With Integrity And Benefited Results';
 export const TAGLINE_DESCRIPTION = 'With unwavering expertise, utmost integrity, and many track record of delivering results, we guide you through your legal complexities';
 export const EMAIL = 'ptarunikaindomiratama@gmail.com';
