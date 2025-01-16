ea220a4c24aa1f5cd3a2dc0995a25ed1c1a71c5e
saddam-satria <ardhisaddam@gmail.com>
2025-01-16 20:33:57 +0700

    feat: gtm

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,12 +2,14 @@ import './index.css';
 import LayoutComponent from '@/components/Layout';
 import { TITLE } from '@/utils/constant';
 import { montserrat } from '@/utils/fonts';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={montserrat.className}>
         <LayoutComponent>{children}</LayoutComponent>
+        <GoogleTagManager gtmId="GTM-P8GK9V7Q" />
       </body>
     </html>
   );
