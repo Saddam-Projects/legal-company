06b35a613dee3a13e284dc85da84414d8d98ffdd
saddam-satria <ardhisaddam@gmail.com>
2025-01-24 21:30:53 +0700

    fix: script js async

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,15 +3,12 @@ import LayoutComponent from '@/components/Layout';
 import { TITLE } from '@/utils/constant';
 import { montserrat } from '@/utils/fonts';
 import { GoogleTagManager } from '@next/third-parties/google';
-import Head from 'next/head';
 import Script from 'next/script';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Head>
-        <Script async type="text/javascript" src="https://nextdev.id/rentetan-script/aim-consultant/rentetan-code.js"></Script>
-      </Head>
+      <Script async type="text/javascript" src="https://nextdev.id/rentetan-script/aim-consultant/rentetan-code.js"></Script>
       <body className={montserrat.className}>
         <LayoutComponent>{children}</LayoutComponent>
         <GoogleTagManager gtmId="GTM-P8GK9V7Q" />
