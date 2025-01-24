b183cd2015d5ddfdb25b99b0a0c1f6e9215fefdc
saddam-satria <ardhisaddam@gmail.com>
2025-01-24 21:07:48 +0700

    feat: wa btn

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,10 +3,18 @@ import LayoutComponent from '@/components/Layout';
 import { TITLE } from '@/utils/constant';
 import { montserrat } from '@/utils/fonts';
 import { GoogleTagManager } from '@next/third-parties/google';
+import Head from 'next/head';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Head>
+        <script
+          type="text/javascript"
+          src="https://nextdev.id/rentetan-script/aim-consultant/rentetan-code.js
+"
+        ></script>
+      </Head>
       <body className={montserrat.className}>
         <LayoutComponent>{children}</LayoutComponent>
         <GoogleTagManager gtmId="GTM-P8GK9V7Q" />
--- frontend/src/components/BottomBar.tsx
+++ frontend/src/components/BottomBar.tsx
@@ -11,7 +11,7 @@ export default function BottomBarComponent({ email, phone }: { email: string; ph
         title: 'Whatsapp',
         name: 'wa',
         color: 'bg-primary',
-        url: `https://wa.me/${phone.replace('0', '62')}`,
+        url: `http://rentetan.nextdigital.co.id/socom?bid=8ff0781eeaa355ce0e36184564fa16d24ff935898cbc8f3a6eb5ba446b745427`,
       },
 
       {
@@ -38,7 +38,9 @@ export default function BottomBarComponent({ email, phone }: { email: string; ph
           <div key={index} className={`flex items-center space-x-1 justify-center bg-primary w-full h-full p-2 ${item.color}`}>
             {item.icon}
             <TextComponent className={`text-black font-medium text-base`}>
-              <a href={item.url}>{item.title}</a>
+              <a className="btn-rentetan" href={item.url}>
+                {item.title}
+              </a>
             </TextComponent>
           </div>
         ))}
